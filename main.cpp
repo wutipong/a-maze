1ac9b3293fc006fc5c3bc7bb24d82f95ceac217f
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2021-11-24 04:28:13 +0700

    integrating maze-gen library

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <raylib.h>
+#include <maze.hpp>
+#include <maze_path.hpp>
 
 int main()
 {
     constexpr int screenWidth = 800;
     constexpr int screenHeight = 450;
 
+    constexpr int from = 50;
+    constexpr int to = 1250;
+
     InitWindow(screenWidth, screenHeight, "raylib-template");
     SetTargetFPS(60);
+
+    auto maze = MazeGen::Generate(50, 50);
+    auto path = MazeGen::FindPathDFS(maze, from, to);
 
     while (!WindowShouldClose())
     {
