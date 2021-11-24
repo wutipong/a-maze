58ec1d099d0984e94dbdde3dc2c3ce0de797da63
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2021-11-24 21:47:07 +0700

    add DrawCell() (incomplete).

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include <raylib.h>
+#include <fmt/core.h>
 #include <maze.hpp>
 #include <maze_path.hpp>
+#include <raylib.h>
+
+using namespace MazeGen;
+
+void DrawCell(Cell &c, int columnId, int rowId);
 
 int main()
 {
@@ -16,12 +21,26 @@ int main()
     auto maze = MazeGen::Generate(50, 50);
     auto path = MazeGen::FindPathDFS(maze, from, to);
 
+    Camera2D camera{.offset = {screenWidth / 2.0f, screenHeight / 2.0f}, .zoom = 1.0f};
     while (!WindowShouldClose())
     {
         BeginDrawing();
         {
-            ClearBackground(RAYWHITE);
-            DrawText("Hello", 190, 200, 20, LIGHTGRAY);
+            ClearBackground(BLACK);
+
+            BeginMode2D(camera);
+            {
+                for (int i = 0; i < maze.cellCount; i++)
+                {
+                    int x = i % maze.column;
+                    int y = i / maze.column;
+
+                    DrawCell(maze[i], x, y);
+                }
+            }
+            EndMode2D();
+
+            DrawCircle(screenWidth / 2, screenHeight / 2, 20, RED);
         }
         EndDrawing();
     }
@@ -29,4 +48,37 @@ int main()
     CloseWindow();
 
     return 0;
+}
+
+void DrawCell(Cell &c, int columnId, int rowId)
+{
+    constexpr int CellWidth = 100;
+    constexpr int CellHeight = CellWidth;
+
+    constexpr int HalfWidth = CellWidth / 2;
+    constexpr int HalfHeight = CellHeight / 2;
+
+    constexpr int WallThickness = 20;
+
+    const int originX = columnId * CellWidth;
+    const int originY = rowId * CellHeight;
+
+    const int cellLeft = originX - HalfWidth;
+    const int cellRight = originX + HalfWidth;
+    const int cellTop = originY - HalfHeight;
+    const int cellBottom = originY - HalfHeight;
+
+    const int cellInnerLeft = cellLeft + WallThickness;
+    const int cellInnerRight = cellRight - WallThickness;
+    const int cellInnerTop = cellTop + WallThickness;
+    const int cellInnerBottom = cellBottom - WallThickness;
+
+    // DrawRectangle(originX - HalfWidth, originY - HalfHeight, CellWidth, CellHeight, DARKGRAY);
+
+    DrawRectangle(cellLeft, cellTop, WallThickness, WallThickness, DARKGREEN);
+    DrawRectangle(cellInnerRight, cellTop, WallThickness, WallThickness, RED);
+    DrawRectangle(cellLeft, cellInnerBottom, WallThickness, WallThickness, DARKBLUE);
+    DrawRectangle(cellInnerRight, cellInnerBottom, WallThickness, WallThickness, DARKGRAY);
+
+    DrawText(fmt::format("{0}, {1}", columnId, rowId).c_str(), originX, originY, 20, LIGHTGRAY);
 }
